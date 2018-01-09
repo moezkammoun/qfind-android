0eef4b0275433f3e9278d7ca73a344c738a17770
Dileep N <dileepn@exalture.com>
2018-01-09 16:12:40 +0530

    commented code removed

--- app/src/main/java/qfind/com/qfindappandroid/categorycontaineractivity/ContainerActivityPresenterImpl.java
+++ app/src/main/java/qfind/com/qfindappandroid/categorycontaineractivity/ContainerActivityPresenterImpl.java
@@ -11,23 +11,14 @@ public class ContainerActivityPresenterImpl {
 
     ContainerActivityView containerActivityView;
 
-//    public ContainerActivityPresenterImpl(ContainerActivityView containerActivityView, Fragment fragment) {
-//        this.containerActivityView = containerActivityView;
-//        containerActivityView.loadFragment(fragment);
-//    }
     public ContainerActivityPresenterImpl(){
 
     }
     public void loadFragmentOncreate(ContainerActivityView containerActivityView, Fragment fragment){
         this.containerActivityView = containerActivityView;
         containerActivityView.loadFragment(fragment);
-
     }
     public void loadFragmentOnButtonClick(Fragment fragment){
         containerActivityView.loadFragment(fragment);
     }
-//    public ContainerActivityPresenterImpl(Fragment fragment){
-//        this.containerActivityView = containerActivityView;
-//
-//    }
 }
