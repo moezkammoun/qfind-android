98c883623f1cf8443cfed3d04669c2ae8426361a
Dileep N <dileepn@exalture.com>
2018-01-21 19:30:28 +0530

    side menu ui completed for both arabic and english

--- app/src/main/java/qfind/com/qfindappandroid/categorycontaineractivity/ContainerActivity.java
+++ app/src/main/java/qfind/com/qfindappandroid/categorycontaineractivity/ContainerActivity.java
@@ -1,6 +1,7 @@
 package qfind.com.qfindappandroid.categorycontaineractivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -14,13 +15,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
-
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import qfind.com.qfindappandroid.InformationPage.InformationPage;
 import qfind.com.qfindappandroid.MainActivity;
 import qfind.com.qfindappandroid.R;
 import qfind.com.qfindappandroid.categoryfragment.CategoryFragment;
@@ -29,16 +29,96 @@ import qfind.com.qfindappandroid.settingspagefragment.SettingsFragment;
 import qfind.com.qfindappandroid.termsandconditionfragment.TermsandConditionFragment;
 
 public class ContainerActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, ContainerActivityView{
+        implements ContainerActivityView {
 
     @BindView(R.id.navigation)
     BottomNavigationView navigation;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @BindView(R.id.sideMenu)
     ImageView hambergerMenu;
+    @BindView(R.id.side_menu_hamburger)
+    ImageView sideMenuHamburger;
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+    @BindView(R.id.side_menu_tittle_txt)
+    TextView sideMenuTittleTxt;
+    @BindView(R.id.side_menu_about_us_txt)
+    TextView sideMenuAboutUsTxt;
+    @BindView(R.id.side_menu_qfinder_txt)
+    TextView sideMenuQfinderTxt;
+    @BindView(R.id.side_menu_terms_condition_txt)
+    TextView sideMenuTermAndConditionTxt;
+    @BindView(R.id.side_menu_contact_us_txt)
+    TextView sideMenuContactUsTxt;
+    @BindView(R.id.side_menu_settings_txt)
+    TextView sideMenuSettingsTxt;
+    @BindView(R.id.about_us_layout)
+    LinearLayout sideMenuAboutUslayout;
+    @BindView(R.id.qfinder_layout)
+    LinearLayout sideMenuQfinderlayout;
+    @BindView(R.id.terms_and_condition_layout)
+    LinearLayout sideMenuTermsandConditionlayout;
+    @BindView(R.id.contact_us_layout)
+    LinearLayout sideMenuContactUslayout;
+    @BindView(R.id.settings_layout)
+    LinearLayout sideMenuSettingslayout;
     ContainerActivityPresenter containerActivityPresenter = new ContainerActivityPresenter();
     Fragment fragment;
-    Menu bottomNavigationMenu;
-    DrawerLayout drawer;
+    //Menu bottomNavigationMenu;
+    public Typeface mtypeFace;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+        ButterKnife.bind(this);
+        toolbar.setNavigationIcon(null);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(false);
+        toggle.syncState();
+        setFontTypeForText();
+        int width = getResources().getDisplayMetrics().widthPixels / 3;
+        DrawerLayout.LayoutParams params = (android.support.v4.widget.DrawerLayout.LayoutParams) navigationView.getLayoutParams();
+        params.width = getResources().getDisplayMetrics().widthPixels - width;
+        navigationView.setLayoutParams(params);
+
+        containerActivityPresenter.loadFragmentOncreate(this, new CategoryFragment());
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.removeShiftMode(navigation);
+        setupHamburgerClickListener();
+        setupSideMenuItemClickListener();
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -47,13 +127,10 @@ public class ContainerActivity extends AppCompatActivity
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.favorite_categories_bottom_menu:
-//                    bottomNavigationMenu.findItem(R.id.qfind_us_menu).setIcon(R.drawable.ic_home_black_24dp);
-//                    bottomNavigationMenu.findItem(R.id.category_history_menu).setIcon(R.drawable.ic_home_black_24dp);
                     fragment = new CategoryFragment();
                     break;
                 case R.id.qfind_us_menu:
-//                    fragment = new CategoryFragment();
-                    Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                     finish();
                     break;
@@ -69,88 +146,99 @@ public class ContainerActivity extends AppCompatActivity
 
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_container);
-        ButterKnife.bind(this);
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setNavigationIcon(null);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        bottomNavigationMenu = navigation.getMenu();
-        bottomNavigationMenu.findItem(R.id.favorite_categories_bottom_menu).setIcon(R.drawable.favourite_icon);
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.setDrawerIndicatorEnabled(false);
-        toggle.syncState();
+    public void drawerOpenCloseHandler() {
+        if (drawer.isDrawerOpen(Gravity.END)) {
+            drawer.closeDrawer(Gravity.END);
+        } else {
+            drawer.openDrawer(Gravity.END);
+        }
+    }
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        // set navigation view width as half of screen
-        int width = getResources().getDisplayMetrics().widthPixels / 3;
-        DrawerLayout.LayoutParams params = (android.support.v4.widget.DrawerLayout.LayoutParams) navigationView.getLayoutParams();
-        params.width = getResources().getDisplayMetrics().widthPixels - width;
-        navigationView.setLayoutParams(params);
-
-        containerActivityPresenter.loadFragmentOncreate(this, new CategoryFragment());
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        BottomNavigationViewHelper.removeShiftMode(navigation);
+    public void setupHamburgerClickListener() {
         hambergerMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (drawer.isDrawerOpen(Gravity.END)) {
-                    drawer.closeDrawer(Gravity.END);
-                } else {
-                    drawer.openDrawer(Gravity.END);
+                drawerOpenCloseHandler();
+            }
+        });
+        sideMenuHamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerOpenCloseHandler();
+            }
+        });
+    }
+
+    public void setFontTypeForText() {
+        if (getResources().getConfiguration().locale.getLanguage().equals("en")) {
+            mtypeFace = Typeface.createFromAsset(getAssets(),
+                    "fonts/Lato-Bold.ttf");
+            sideMenuTittleTxt.setTypeface(mtypeFace);
+            mtypeFace = Typeface.createFromAsset(getAssets(),
+                    "fonts/Lato-Light.ttf");
+        } else {
+            mtypeFace = Typeface.createFromAsset(getAssets(),
+                    "fonts/GE_SS_Unique_Light.otf");
+        }
+        sideMenuAboutUsTxt.setTypeface(mtypeFace);
+        sideMenuQfinderTxt.setTypeface(mtypeFace);
+        sideMenuTermAndConditionTxt.setTypeface(mtypeFace);
+        sideMenuContactUsTxt.setTypeface(mtypeFace);
+        sideMenuSettingsTxt.setTypeface(mtypeFace);
+    }
+
+    public void setupSideMenuItemClickListener() {
+        sideMenuAboutUslayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                fragment = new SearchResultsFragment();
+                containerActivityPresenter.loadFragmentOnButtonClick(fragment);
+                drawer.closeDrawer(GravityCompat.END);
+            }
+        });
+        sideMenuQfinderlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                drawer.closeDrawer(GravityCompat.END);
+            }
+        });
+        sideMenuTermsandConditionlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Fragment f = getSupportFragmentManager().findFragmentById(R.id.frame_container);
+                if (!(f instanceof TermsandConditionFragment)) {
+                    fragment = new TermsandConditionFragment();
+                    containerActivityPresenter.loadFragmentOnButtonClick(fragment);
                 }
+
+                drawer.closeDrawer(GravityCompat.END);
+            }
+        });
+        sideMenuContactUslayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                drawer.closeDrawer(GravityCompat.END);
+            }
+        });
+        sideMenuSettingslayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Fragment f = getSupportFragmentManager().findFragmentById(R.id.frame_container);
+                if (!(f instanceof SettingsFragment)) {
+                    fragment = new SettingsFragment();
+                    containerActivityPresenter.loadFragmentOnButtonClick(fragment);
+                }
+
+                drawer.closeDrawer(GravityCompat.END);
             }
         });
 
-
     }
 
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.END)) {
-            drawer.closeDrawer(GravityCompat.END);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.about_us_menu_item) {
-            fragment = new SearchResultsFragment();
-            containerActivityPresenter.loadFragmentOnButtonClick(fragment);
-        } else if (id == R.id.qfinder_menu_item) {
-
-        } else if (id == R.id.terms_menu_item) {
-            fragment = new TermsandConditionFragment();
-            containerActivityPresenter.loadFragmentOnButtonClick(fragment);
-        } else if (id == R.id.contact_menu_item) {
-
-        } else if (id == R.id.settings_menu_item) {
-            fragment = new SettingsFragment();
-            containerActivityPresenter.loadFragmentOnButtonClick(fragment);
-        }
-        drawer.closeDrawer(GravityCompat.END);
-        return true;
-    }
-
-    @Override
-    public void loadFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
 
 }
